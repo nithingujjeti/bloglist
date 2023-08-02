d4a15ae064b2f047ed0475faf2e14ca4b2f847fe
nithin <nithingujjeti77@gmail.com>
2023-08-02 18:22:21 +0000

    solution

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="userName-container">
+    <div className="user-info">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+        alt="profile"
+        className="userImage"
+      />
+      <h1 className="profile-heading">Wade Warren</h1>
+      <p className="description">Software developer at UK</p>
+    </div>
+  </div>
+)
+
+export default UserInfo
